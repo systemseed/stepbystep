1e84add192c1457c066ffc2029b154c88d8e654c
Alexey Beloglazov <beloglazov91@gmail.com>
2022-11-30 23:03:24 +0300

    Update Anu LMS up to newest version.

--- modules/sbs_sessions/src/Normalizer/CourseNormalizer.php
+++ modules/sbs_sessions/src/Normalizer/CourseNormalizer.php
@@ -2,43 +2,67 @@
 
 namespace Drupal\sbs_sessions\Normalizer;
 
+use Drupal\anu_lms\Course;
+use Drupal\anu_lms\CourseProgress;
 use Drupal\anu_lms\Normalizer\CourseNormalizer as AnuCourseNormalizer;
 
 /**
  * Converts Course node object structure to a JSON array structure.
  */
 class CourseNormalizer extends AnuCourseNormalizer {
-    /**
-     * {@inheritdoc}
-     */
-    public function normalize($entity, $format = NULL, array $context = []) {
-        $normalized = parent::normalize($entity, $format, $context);
 
-        $courseProgressHandler = \Drupal::service('anu_lms.course_progress');
-        $courseHandler = \Drupal::service('anu_lms.course');
+  /**
+   * The course page service.
+   *
+   * @var \Drupal\anu_lms\Course
+   */
+  protected Course $course;
 
-        $normalized['content_urls'] = $courseHandler->getLessonsAndQuizzesUrls($entity);
+  /**
+   * The course progress manager.
+   *
+   * @var \Drupal\anu_lms\CourseProgress
+   */
+  protected CourseProgress $courseProgress;
 
-        if ($courseHandler->isLinearProgressEnabled($entity)) {
-            $normalized['progress'] = $courseProgressHandler->getCourseProgress($entity);
+  /**
+   * Constructs an object.
+   *
+   * @param \Drupal\anu_lms\Course $course
+   *   The Course service.
+   * @param \Drupal\anu_lms\CourseProgress $course_progress
+   *   The Course progress handler.
+   */
+  public function __construct(Course $course, CourseProgress $course_progress) {
+    $this->course = $course;
+    $this->courseProgress = $course_progress;
 
-            // If progress is enabled, we disable all REST Entity Recursive cache!
-            // @todo extract progress data in a separate data attribute and enable
-            // back cache.
-            if (isset($context[static::SERIALIZATION_CONTEXT_CACHEABILITY])) {
-                /** @var \Drupal\Core\Cache\CacheableMetadata $cacheable_metadata */
-                $cacheable_metadata = $context[static::SERIALIZATION_CONTEXT_CACHEABILITY];
-                $cacheable_metadata->setCacheMaxAge(0);
-            }
+  /**
+   * {@inheritdoc}
+   */
+  public function normalize($entity, $format = NULL, array $context = []) {
+    $normalized = parent::normalize($entity, $format, $context);
+
+    $normalized['content_urls'] = $this->course->getLessonsAndQuizzesUrls($entity);
+
+    if ($this->course->isLinearProgressEnabled($entity)) {
+      $normalized['progress'] = $this->courseProgress->getCourseProgress($entity);
+
+      // If progress is enabled, we disable all REST Entity Recursive cache!
+      // @todo extract progress data in a separate data attribute and enable
+      // back cache.
+      if (isset($context[static::SERIALIZATION_CONTEXT_CACHEABILITY])) {
+        /** @var \Drupal\Core\Cache\CacheableMetadata $cacheable_metadata */
+          $cacheable_metadata = $context[static::SERIALIZATION_CONTEXT_CACHEABILITY];
+          $cacheable_metadata->setCacheMaxAge(0);
         }
+      }
 
-        if (isset($context['course_page_categories']) &&
-            $courseProgressHandler->isLocked($entity, $context['course_page_categories'])
-        ) {
-            $normalized['locked'] = ['value' => TRUE];
-        }
+      if (isset($context['course_page_categories']) && $this->courseProgress->isLocked($entity, $context['course_page_categories'])) {
+        $normalized['locked'] = ['value' => TRUE];
+      }
 
-        return $normalized;
+      return $normalized;
     }
 
 }

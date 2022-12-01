c8fc55d11f61e01375a6defcb027402a552ac34d
Alexey Beloglazov <beloglazov91@gmail.com>
2022-12-01 12:07:36 +0300

    Update Anu LMS up to newest version.

--- tests/src/FunctionalJavascript/SessionHealthCheckTest.php
+++ tests/src/FunctionalJavascript/SessionHealthCheckTest.php
@@ -2,6 +2,7 @@
 
 namespace Drupal\Tests\stepbystep\FunctionalJavascript;
 
+use Drupal\Core\Url;
 use Drupal\FunctionalJavascriptTests\WebDriverTestBase;
 use Drupal\user\Entity\User;
 
@@ -25,23 +26,47 @@ class SessionHealthCheckTest extends WebDriverTestBase {
   protected $strictConfigSchema = FALSE;
 
   /**
+   * {@inheritdoc}
+   */
+  protected function drupalLogin($account, $password = '') {
+    $pass = $password ? $password : $this->rootUser->pass_raw;
+    $this->drupalGet(Url::fromRoute('user.login'));
+    $this->submitForm([
+      'name' => $account->getEmail(),
+      'pass' => $pass,
+    ], 'Log in', 'user-login-form');
+
+    $account->sessionId = $this->getSession()->getCookie(\Drupal::service('session_configuration')->getOptions(\Drupal::request())['name']);
+
+    $this->assertTrue($this->drupalUserIsLoggedIn($account), "User 1 is logged in.");
+
+    $this->loggedInUser = $account;
+    $this->container->get('current_user')->setAccount($account);
+  }
+
+  /**
    * Make sure the first session can be opened in browser.
    */
   public function testSessions() {
-    // Login as 2nd user as it's already assigned to the demo storyline.
-    $account = User::load(2);
-    $this->container->get('current_user')->setAccount($account);
-
-    $this->drupalGet('lesson/welcome');
+    // Login as first user as it's already assigned to the demo storyline.
+    $account = User::load(1);
+    $account->activate();
+    $account->save();
+    $this->drupalLogin($account);
 
     $assert = $this->assertSession();
     // Check for the first session and open it.
     $assert->waitForText('Take the first step!');
+    $assert->waitForElementVisible('css', 'div.MuiPaper-root:nth-child(1) > a:nth-child(1)')->click();
 
     // Check for the heading of the first lesson.
     $lesson_heading = $assert->waitForElementVisible('css', 'h6.MuiTypography-root');
     $this->assertNotEmpty($lesson_heading);
     $this->assertEquals($lesson_heading->getText(), 'Welcome');
+
+    // Block the first user again.
+    $account->block();
+    $account->save();
   }
 
 }
